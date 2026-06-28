a41d602c087eba99db99780059927bc397a7dd5f
mquynh26 <mquynh150305@gmail.com>
2026-06-28 17:03:28 +0000

    update file connect database

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "webuser";
+$password = "123456";
 $dbname = "datlichthethao";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
